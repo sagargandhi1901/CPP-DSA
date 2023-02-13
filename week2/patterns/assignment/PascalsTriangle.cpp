dbfbbb29c8b15b9818658d04ed3c8a7b76747fe2
Sagar Gandhi <sagargandhi968@gmail.com>
2023-02-13 20:40:05 +0530

    Pascal's triangle lecture 4 week 2 assignment

--- week2/patterns/assignment/PascalsTriangle.cpp
+++ week2/patterns/assignment/PascalsTriangle.cpp
@@ -6,5 +6,15 @@ int main() {
     int n;
     cout << "Enter value of n : " << endl;
     cin >> n;
+    int number = 1;
 
+    for (int row = 0; row < n; row++) {
+
+        number = 1;
+        for (int col = 0; col <= row; col++) {
+            cout << number << " ";
+            number = number * (row - col) / (col + 1);
+        }
+        cout << endl;
+    }
 }
